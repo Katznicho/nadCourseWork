727ffb2147f8f79b3013faea096612231c694478
katznicho <katznicho@gmail.com>
2021-12-04 22:01:47 +0300

    end-no-edit

--- build/web/PrintCertificate.jsp
+++ build/web/PrintCertificate.jsp
@@ -9,7 +9,24 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Certificate of Vaccination</title>
+        <style>
+                .center{
+                        display: grid;
+                        place-items: center;
+                }
+                .table{
+                        display: flex;
+                        align-items: center;
+                        justify-content: space-around;
+                        border-bottom:1px solid #000;
+                        width: 90%;
+                        margin-left: 5%;
+                        margin-right: 10%;
+                        
+                }
+
+        </style>
     </head>
     <body>
               <%@ taglib uri="/WEB-INF/tlds/GetCovidInfo" prefix="Print" %>
@@ -17,6 +34,25 @@
             String id = request.getParameter("name");
             
         %>
-        <Print:printCertificate table="patients" values="<%= id %>"></Print:printCertificate>
+        <div class="container">
+              <div class="center">
+                        <h1>Vaccination Certificate</h1>
+                </div>
+                <div class="center">
+                        <img src="https://lh3.googleusercontent.com/proxy/V5z_y0EZb1EhimUgrkuNIs_zgO9ihRseXpwMGBsjWcepnIaGm3rOSSPg_X8i-eYfSmDDOYC5nFTaG9JPkpDsq1vvrjUMlodHT7DYs88" 
+                        alt="">
+                </div>
+                    <Print:printCertificate table="patients" values="<%= id %>"></Print:printCertificate>
+                    <div style="display:grid;place-items: center; padding: 20px;">
+<!--                        <button 
+                        style="color: #fff; background-color: #1c478e;border: 0;
+                        cursor: pointer;width: 25%; height: 35px;border-radius: 20px;;"> Back</button>-->
+<a  href="ShowPatients.jsp">Back</a>
+
+                 </div>
+        </div>
+
+        
+        
     </body>
 </html>
--- src/java/org/health/administration/printCertificate.java
+++ src/java/org/health/administration/printCertificate.java
@@ -48,17 +48,76 @@ public class printCertificate extends SimpleTagSupport {
                   ResultSet vaccineType  = St.executeQuery("select * from patients where patientName = '"+values+"'");
     while(vaccineType.next()){
 
-    out.println("  <tr>\n" +
-"      <th scope=\"row\">"+vaccineType.getString("patientsId")+"</th>\n" +
-        "      <th scope=\"row\">"+vaccineType.getString("patientName")+"</th>\n" +
-"      <td>"+vaccineType.getString("healthCenterName")+"</td>\n" +
-"      <td>"+vaccineType.getString("batchNumber")+"</td>\n" +
-"      <td> "
-        + ""+vaccineType.getString("NIN")+"</td>\n" +
-            "<td><button onClick='window.location=\"PrintCertificate.jsp?name="+vaccineType.getString("patientName")+"\"'>view certificate</button></td>\n" +
-"    </tr>");
+    out.println(" "
+            + "<div style=\"display: flex;\n" +
+"                align-items: center;\n" +
+"                justify-content: space-around;\n" +
+"                border-bottom:1px solid #000;\n" +
+"                width: 90%;\n" +
+"                margin-left: 5%;\n" +
+"                margin-right: 10%;\">\n" +
+"                        <h4>Name</h4>\n" +
+"                        <h4>"+vaccineType.getString("patientName")+"</h4></div>"
+                    + "<div style=\"display: flex;\n" +
+"                align-items: center;\n" +
+"                justify-content: space-around;\n" +
+"                border-bottom:1px solid #000;\n" +
+"                width: 90%;\n" +
+"                margin-left: 5%;\n" +
+"                margin-right: 10%;\">\n" +
+"                        <h4>Health Center</h4>\n" +
+"                        <h4>"+vaccineType.getString("healthCenterName")+"</h4></div>"
+        + "<div style=\"display: flex;\n" +
+"                align-items: center;\n" +
+"                justify-content: space-around;\n" +
+"                border-bottom:1px solid #000;\n" +
+"                width: 90%;\n" +
+"                margin-left: 5%;\n" +
+"                margin-right: 10%;\">\n" +
+"                        <h4>NIN Number</h4>\n" +
+"                        <h4>"+vaccineType.getString("NIN")+"</h4></div>"
+        + "<div style=\"display: flex;\n" +
+"                align-items: center;\n" +
+"                justify-content: space-around;\n" +
+"                border-bottom:1px solid #000;\n" +
+"                width: 90%;\n" +
+"                margin-left: 5%;\n" +
+"                margin-right: 10%;\">\n" +
+"                        <h4>Date of Vaccination</h4>\n" +
+"                        <h4>"+vaccineType.getString("dateOfVaccination")+"</h4></div>"
+        + "<div style=\"display: flex;\n" +
+"                align-items: center;\n" +
+"                justify-content: space-around;\n" +
+"                border-bottom:1px solid #000;\n" +
+"                width: 90%;\n" +
+"                margin-left: 5%;\n" +
+"                margin-right: 10%;\">\n" +
+"                        <h4>Type of Vaccination</h4>\n" +
+"                        <h4>"+vaccineType.getString("typeOfVaccine")+"</h4></div>"
+        + "<div style=\"display: flex;\n" +
+"                align-items: center;\n" +
+"                justify-content: space-around;\n" +
+"                border-bottom:1px solid #000;\n" +
+"                width: 90%;\n" +
+"                margin-left: 5%;\n" +
+"                margin-right: 10%;\">\n" +
+"                        <h4>Batch Number</h4>\n" +
+"                        <h4>"+vaccineType.getString("batchNumber")+"</h4></div>"
+        + "<div style=\"display: flex;\n" +
+"                align-items: center;\n" +
+"                justify-content: space-around;\n" +
+"                border-bottom:1px solid #000;\n" +
+"                width: 90%;\n" +
+"                margin-left: 5%;\n" +
+"                margin-right: 10%;\">\n" +
+"                        <h4>Return Date</h4>\n" +
+"                        <h4>"+vaccineType.getString("returnDate")+"</h4></div>"
+        + "");
 
 }
+     out.println("<div style=\"display:grid;place-items: center; padding: 20px;\">"
+             + "<button onClick='window.print()' style=\"color: #fff; background-color: #1c478e;border: 0; cursor: pointer;width: 25%; height: 35px;border-radius: 20px;;\">Print Certificate</button>"
+             + "</div>");
 
     
     Conn.closeConnection();
